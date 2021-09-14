8d2d586c554ab40059d2f2864d65a03c24cd2733
Shariful Islam <tulip.du@gmail.com>
2021-09-14 20:40:31 +0600

    Add application plugin and file tree in Readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # javalin-vue-kotlin
+
+File Structure:
+
+![alt text](readme-resource/javalin-vue-files.JPG)
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,13 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.10"
+    application
 }
+
+application {
+    mainClass.set("com.nomad.javalin.MainKt")
+}
+
 group = "me.nomad"
 version = "1.0-SNAPSHOT"
 
